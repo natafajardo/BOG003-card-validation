3dea20c29c9d341583db4f4411bff43eeb0be3ed
natafajardo <79486039+natafajardo@users.noreply.github.com>
2021-06-25 16:51:01 -0500

    maquetacion pantalla 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+const formularioTarjeta = document.querySelector('#formularioTarjeta');
+
+
+
+
+
+formularioTarjeta.inputNumero.addEventListener('keyup' , (e) => {
+    let valorInput = e.target.value;
+
+    formularioTarjeta.inputNumero.value = valorInput
+    .replace(/\s/g, '')
+    .replace(/\D/g, '')
+    .trim();
+});
+
+
+function validar (){
+    var array = validar.split("");
+
+
+}
+
+
+
+
+
+
+
+
